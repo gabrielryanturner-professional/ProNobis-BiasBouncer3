2330bd966fc8192b924eeb9af1a55d1a9669353e
gabrielryanturner-professional <gabe@gabrielryanturner.com>
2025-09-07 07:37:37 +0000

    UI Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
 st.title("BiasBouncer")
-prompt = st.chat_input("Say something")
-if prompt:
+prompt = st.chat_input("Type 'Create Team' to create a team")
+if prompt == "Create Team":
+    col1, col2, col3 = st.columns(3, border=True, width="stretch")
+    with col1:
+        st.write("Member 1")
+    with col2:
+        st.write("Member 2")
+    with col3:
+        st.write("Member 3")
+
+else:
     st.write(f"User has sent the following prompt: {prompt}")
