adae6b09882c3cc78fb9b9693d6a1cbbe7b4975a
gabrielryanturner-professional <gabe@gabrielryanturner.com>
2025-09-07 07:47:28 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,17 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.markdown("<h1 style='text-align: center;'><span style='color: red;'>BiasBouncer</span></h1>", unsafe_allow_html=True)
-prompt = st.chat_input("Type 'Create Team' to create a team")
-if prompt == "Create Team":
-    col1, col2, col3 = st.columns(3, border=True, width="stretch")
-    with col1:
-        st.subheader("Member 1")
-    with col2:
-        st.subheader("Member 2")
-    with col3:
-        st.subheader("Member 3")
-else:
-    st.write(f"User has sent the following prompt: {prompt}")
+messages_container = st.container(height=575, border=None)
+
+with messages_container:
+    for msg in st.session_state["chat_history"]:
+        role = msg["role"]
+        content = msg["content"]
+
+        if role == "user":
+            st.chat_message("user").write(content)
+        else:
+            st.chat_message("assistant").write(f"**{role}**: {content}")
+
+
+user_input = st.chat_input("Work with the Agents")
