bf8a181417244e9a14eeb2d5698f8e23565f2c57
gabrielryanturner-professional <gabe@gabrielryanturner.com>
2025-09-07 07:48:02 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
 st.markdown("<h1 style='text-align: center;'><span style='color: red;'>BiasBouncer</span></h1>", unsafe_allow_html=True)
 messages_container = st.container(height=575, border=None)
 
