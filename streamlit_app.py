13c5270f130414ea0533b71946a45e7853873090
gabrielryanturner-professional <gabe@gabrielryanturner.com>
2025-09-08 04:30:00 +0000

    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.markdown("<h1 style='text-align: center;'><span style='color: red;'>BiasBounc
 
 prompt = st.chat_input("Create Team")
 
-mock_answer = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+mock_answer = "This is a mock AI answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 def stream_data():
     for word in mock_answer.split(" "):
@@ -13,6 +13,11 @@ def stream_data():
         time.sleep(0.02)
 
 if prompt == "Create Team":
+    with st.status("Creating Team..."):
+        st.write("Doing Research...")
+        time.sleep(2)
+        st.write("Finalizing Roles...")
+        time.sleep(2)
     col1, col2, col3 = st.columns(3, border=True)
     with col1:
         st.write("Member 1")
