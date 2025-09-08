c33bea5a31655cf8211947dfad684536e5dfc885
gabrielryanturner-professional <gabe@gabrielryanturner.com>
2025-09-08 04:25:52 +0000

    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,10 @@ st.markdown("<h1 style='text-align: center;'><span style='color: red;'>BiasBounc
 
 prompt = st.chat_input("Create Team")
 
+mock_answer = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 def stream_data():
-    for word in prompt.split(" "):
+    for word in mock_answer.split(" "):
         yield word + " "
         time.sleep(0.02)
 
