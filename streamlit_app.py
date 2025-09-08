52dc85568059b8207a757ead331ea3a59a3b0a41
gabrielryanturner-professional <gabe@gabrielryanturner.com>
2025-09-08 04:18:35 +0000

    Revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,16 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
-
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
 
 st.markdown("<h1 style='text-align: center;'><span style='color: red;'>BiasBouncer</span></h1>", unsafe_allow_html=True)
-messages_container = st.container(height=575, border=None)
 
-with messages_container:
-    for msg in st.session_state["chat_history"]:
-        role = msg["role"]
-        content = msg["content"]
-
-        if role == "user":
-            st.chat_message("user").write(content)
-        else:
-            st.chat_message("assistant").write(f"**{role}**: {content}")
-
-
-user_input = st.chat_input("Work with the Agents")
+prompt = st.chat_input("Create Team")
+if prompt == "Create Team":
+    col1, col2, col3 = st.columns()
+    with col1:
+        st.write("Member 1")
+    with col2:
+        st.write("Member 2")
+    with col3:
+        st.write("Member 3")
+else:
+    st.write(f"User said: {prompt}")
